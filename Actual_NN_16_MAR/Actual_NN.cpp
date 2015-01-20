7d5bfac8e1f9929ee1612f0910d84bd1165f9331
Cheryl Dsouza <cvd3@pitt.edu>
2015-01-20 15:39:39 -0500

    1. Actual_NN- Exact Algorithm 2.Brute Force- Heuristic Algorithm

---
+++ Actual_NN_16_MAR/Actual_NN.cpp
@@ -0,0 +1,155 @@
+// The Nearest Neighbor Algorithm
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+
+int numCities ;
+int **cityDistances ;
+int *cityPathTaken ;
+double totalDistance  ;
+
+double FindShortestPath_NN()  
+{  
+  clock_t start, end;  
+ 
+ 
+  // recording the  start time of algorithm
+ start = clock();  
+ cout << " " << endl << endl ;
+ bool *seen = new bool[numCities + 1] ;
+
+ // Will start from first city by default  
+ int cityVisited = 1;  
+   
+ int minDistance = INT_MAX;  
+ 
+ int curr = 1;  
+ int j;  
+ int minDistanceCity = 0;  
+ 
+ // Traversing through all the cities to find shortest path approximation
+ while(cityVisited <= numCities)  
+ {  
+   
+  // Iterate through all cites near minDistanceCity city   
+  for(j=1; j<=numCities;j++)  
+  {  
+   // Checking if the distance from cityPathTakenent city to neighbour is lower then the minDistance   
+   if(cityDistances[curr][j] < minDistance)  
+   {  
+    // if this city is not already visited
+    if(!seen[j])  
+    {  
+     // recording the new minDistance  
+     minDistance = cityDistances[curr][j];  
+     minDistanceCity = j;  
+    }  
+   }  
+  }  
+
+   totalDistance += minDistance ;
+  cityPathTaken[cityVisited] = minDistanceCity;   
+
+ 
+  // set the cityPathTakenent city as already seen  
+   seen[minDistanceCity] = true;  
+  
+  // set minDistanceCity as next base city for shortest path calculation
+  curr = minDistanceCity;  
+  
+  // reset minDistance to INT_MAX for the next round  
+  minDistance = INT_MAX;  
+
+
+  // increment cityVisited counter  
+  cityVisited++;  
+ }  
+
+
+ // Add retour to the first city  
+ cityPathTaken[cityVisited] = cityPathTaken[1];  
+ 
+ //add the distance of the last minDistanceCity back to the first city
+ totalDistance += cityDistances[minDistanceCity][1] ;
+  
+ delete []seen ;
+ 
+
+ // record end time of algorithm
+
+ end = clock();  
+ 
+//calculate time required in milliSeconds 
+ double ct =((end-start)/ ((double)CLOCKS_PER_SEC)) * 1000.0;  
+ 
+ return ct;  
+
+}  
+
+int main()
+{
+	
+
+	double timeTaken ;
+
+	std::cout << " Enter number of cities " << endl ;
+	std::cin >> numCities ;
+
+	cityPathTaken = new int[numCities +1];	
+	cityDistances = new int*[numCities+1];
+
+ 	// allocating memory to the city Distance matrix
+	for(int i = 0; i <= numCities; ++i)
+    	cityDistances [i] = new int[numCities+1];
+
+	
+ 	for (int s=0; s<=numCities; s++) {
+	    for (int r=0; r<=numCities; r++) {
+			if (s != r) {
+				
+				cityDistances[s][r]= rand()%25+1; //random function is used to generate the distance matrix from 1-25.
+				cityDistances[r][s]= cityDistances[s][r];				
+			
+			}
+			else
+				cityDistances[s][r] = 0 ;
+		}
+	}
+	
+
+	cout << "Distance matrix" << endl ;
+ 	for (int s=1; s<=numCities; s++)  {
+	    for (int r=1; r<=numCities; r++)  {
+			
+			cout << cityDistances[s][r] << "\t" ;			 
+		}
+		cout << endl ;
+	}
+
+	timeTaken = FindShortestPath_NN() ;
+
+	cout << "Total distance traversed " << totalDistance << endl << endl ; // printing the total distance traversed
+	cout << "Shortest Path Approximation " << endl ; //printing the shortest path
+	for(int i = 1; i <= numCities+1; ++i)
+		cout << cityPathTaken[i] << "  " ;
+	
+	cout << endl ;
+
+
+
+
+	for(int i = 0; i <= numCities; ++i)
+    	delete cityDistances [i] ;
+
+	delete[] cityDistances;
+
+	//delete[] cityPathTaken ;
+	
+	return 0 ;
+	
+}
---
+++ bruteForce_1_16_Mar/bruteForce_1.cpp
@@ -0,0 +1,112 @@
+
+//2.AND 3. Source code for BRUTE FORCE
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <math.h>
+ 
+using namespace std;
+
+int *distanceArr ;
+
+void tsp(int numCities,  char* nodes) ;
+
+int main() {
+ 
+    int numCities; //declared the number of nodes (cities)
+    cout << "Please enter the numbers of nodes: " << endl ;
+    cin >> numCities ;
+ 
+	// declaring x-axis and y-axis of graph
+ 	distanceArr = new int[numCities * numCities];
+
+    char* nodes  = new char[numCities];
+ 
+    // generating random x and y values for each point
+    int i, j;
+    srand(time(NULL));
+    for(i=0; i<numCities; i++) {
+        nodes[i] = i;
+
+		for ( j = 0; j < numCities; j++ )
+		{
+			if (i != j) {
+				// map distances between two nodes to a 1-D array
+				distanceArr[j+(i*numCities)] = rand()%25 + 1;             //rand function is used to randomly generate the matrix with a limit of 1-25.
+				distanceArr[i+(j*numCities)] =  distanceArr[j+(i*numCities)] ;
+			}
+			else {
+				distanceArr[j+(i*numCities)] = 0 ;
+
+			}
+				
+			
+		}
+
+	}
+		
+		
+
+	cout << "Distance Matrix " << endl << endl ; // printing the distance matrix
+    for(i=0; i<numCities; i++) {
+		for ( j = 0; j < numCities; j++ ) {
+			cout << distanceArr[j+(i*numCities)]  << "\t" ;
+		}
+		cout << endl ;
+	}
+ 
+    tsp(numCities,nodes);
+ 
+	delete []distanceArr ;
+    return 0;
+} 
+
+void tsp(int numCities, char* nodes) {
+    int tmpcost, shortest_path;
+    int cheap_path[numCities];
+    int i, i1, i2  ;
+	
+	shortest_path = INT_MAX ;
+ 
+    //PERMUTATIONS
+    do {
+        tmpcost = 0;
+       
+
+			i1 = numCities - 1;
+			for ( i2 = 0; i2 < numCities; i2++ )
+			{
+			  tmpcost = tmpcost + distanceArr[nodes[i1]+(nodes[i2]*numCities)];
+			  i1 = i2;
+			}
+		
+		
+        if( shortest_path!=0 && shortest_path > tmpcost ) {  // checking for the shortest path
+            shortest_path = tmpcost;
+            for (i=0; i<numCities; i++) {
+                cheap_path[i] = nodes[i];
+            }
+        }
+        
+    } while(next_permutation(nodes,nodes+numCities));
+ 
+    //PRINTING THE RESULTS
+	cout << endl ;
+    printf("RESULTS\n The shortest path is : ");
+    for (i=0; i<numCities; i++) {
+        cout << cheap_path[i] << "\t";
+    }
+	cout << cheap_path[0] << endl ;
+
+	
+    cout << "Shortest path is " << shortest_path << endl ; // printing the shortest path
+	
+	
+}
+ 
